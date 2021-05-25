6ebbe5d18028f91bb2ae971c92a02afd049ca419
Gabe Ambrosio <ambrosiogabe@gmail.com>
2021-05-25 16:41:06 -0500

    Update

--- docs/html/custom.js
+++ docs/html/custom.js
@@ -1,10 +1,12 @@
 window.onload = function () {
-    var theme, prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
-    if (prefersDarkScheme.matches)
-        theme = document.body.classList.contains("light-mode") ? "light" : "dark";
-    else
-        theme = document.body.classList.contains("dark-mode") ? "dark" : "light";
+    var theme = localStorage.getItem("theme") == null ? "light" : localStorage.getItem("theme");
+    if (theme == "dark") {
+        document.getElementById('darkModeToggle').checked = true;
+    } else {
+        document.getElementById('darkModeToggle').checked = false;
+    }
     localStorage.setItem("theme", theme);
+    detectColorScheme(theme);
 }
 
 //determines if the user has a set theme
